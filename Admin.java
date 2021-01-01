64351f0f8fbb8105d52abedbc26330dff223e12b
zhengwei210 <54303708+zhengwei210@users.noreply.github.com>
2021-01-01 17:11:36 +0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,19 +3,31 @@ public class Admin extends Person {
     Admin() {}
 
     public void ChangeCustomerDetail() {
-
+    
     }
 
     public void DisplayCustomerDetails() {
-
+        System.out.print("CUSTOMER LIST");
+        System.out.println("-------------------------");
+        System.out.format("No\t" + "Name\t" + "Phone\t" + "Status");
+        System.out.println("");
+        System.out.println("-------------------------")
     }
     
     public void DisplayShopDetails() {
-
+        System.out.print("SHOP LIST");
+        System.out.println("-------------------------");
+        System.out.format("No\t" + "Name\t" + "Phone\t" + "Manager\t" + "Status");
+        System.out.println("");
+        System.out.println("-------------------------");
     }
 
     public void MasterVisitHistory() {
-
+        System.out.print("MASTER VISIT HISTORY");
+        System.out.println("-------------------------");
+        System.out.format("No\t" + "Date\t" + "Time\t" + "Customer\t" + "Shop");
+        System.out.println("");
+        System.out.println("-------------------------");
     }
     
     
