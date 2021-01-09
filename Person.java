16323a8a54997d52d5e8e8e08b9b2f7aa0bda139
zhengwei210 <54303708+zhengwei210@users.noreply.github.com>
2021-01-09 21:27:20 +0800

    Update Person.java
    Javadoc added

--- Person.java
+++ Person.java
@@ -1,78 +1,63 @@
-//import org.json.*;
-
+/**
+ * Person represents customers' data such as their
+ * name, phone and status. This class were extended
+ * to Customer and Admin to let these programs to
+ * get data easily
+ * @author Fong
+ */
 public class Person {
-    
-    public String name;
+
+    private String name;
     private String phone;
     private String[] statuslist = {"Normal", "Case", "Close"};
     private String status;
-    //Vector<String> v = new Vector<>();
 
     Person() {}
-
+    /**
+     * This method constructs a customer with a specified name and phone
+     * @param name - The name value of Customer
+     * @param phone - The status value of Customer
+     */
     Person(String name,String phone) {
         this.name = name;
         this.phone = phone;
         this.status = statuslist[0];
     }
     
+    /**
+     * This method constructs a customer with a specified name, phone and status
+     * @param name - The name value of Customer
+     * @param phone - The phone value of Phone
+     * @param status - The status value of Status
+     */
     Person(String name,String phone, String status) {
         this.name = name;
         this.phone = phone;
         this.status = status;
     }
 
+    /**
+     * Return the name from the specified customer
+     * @return - name in Customer
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Return the phone from the specified customer
+     * @return - phone in Customer
+     */
     public String getPhone(){
         return phone;
     }
 
+    /**
+     * Return the status from the specified customer
+     * @return - status in Customer
+     */
     public String getStatus(){
         return status;
     }
-
-   /*
-    public void PersonToJSON() {
-        JSONObject people = new JSONObject();
-        
-        people.put("name",name);
-        people.put("phone",phone);
-        people.put("status",status);
-
-        String filename = "saves.json";
-
-        try (FileWriter file = new FileWriter(filename)) {
-            file.write(people.toString());
-        }
-
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
-*/
-
-    //Customer shenenigans to move to a new class
-    private void checkIn(String date, String time, String location) {
-        
-        System.out.println("Under construction");
-        //Add to CSV
-    }
-
-    public void checkStatus() {
-        
-    }
-
-    public void checkLocation() {
-
-    }
-
-
-
-
-
-
-    
+  
 }
